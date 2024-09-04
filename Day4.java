38fbea1f956757cfb320c6749cd8a03a82f9ac3d
Puputyunitaa <puputyunita1775@gmail.com>
2024-09-04 19:10:01 +0800

    Membuat System.out.println

---
+++ Day4.java
@@ -0,0 +1,7 @@
+public class Day3 {
+    public static void main(String[] args) {
+        System.out.println("NAMA: PUPUT YUNITA");
+        System.out.println("PRODI: INFORMATIKA");
+            System.out.println("FAKULTAS: TEKNIK");
+}
+}
