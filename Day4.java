776fbaff5df145d19f2d0c21570a90d68bb6e1ff
Puputyunitaa <puputyunita1775@gmail.com>
2024-09-05 20:12:21 +0800

    Membuat System.out.print dan System.out.printf

---
+++ Day4.java
@@ -0,0 +1,8 @@
+public class D4 {
+    public static void main(String[] args) {
+        System.out.print("INFORMATIKA");
+        System.out.printf("%nNama : %S %nUmur ku 19 : %b", "puput yunita",19 );
+        
+    }
+    
+}
