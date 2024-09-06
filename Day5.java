8d3cae4fd3420290d0821ad31435b22411da7612
Puputyunitaa <puputyunita1775@gmail.com>
2024-09-06 21:30:07 +0800

    membuat komentar

---
+++ Day5.java
@@ -0,0 +1,16 @@
+public class D5 {
+    //method main adalah di mana sebuah program di eksekusi dan berjalan didalamnya//
+    public static void main(String[] args) {
+    //println di gunakan untuk mencetak output ke baris baru //    
+        System.out.println("NAMA : PUPUT YUNITA");
+    //print di gunakan untuk mencetak output ke baris yang sama//
+        System.out.print("INFORMATIKA");
+    //printf di gunakan untuk mencetak output denga format yang lebih spesifik//
+    /*1. %n di gunakn dalam printf untuk mencetak ke baris selanjutnya
+      2. %S di gunakan untuk mencetak tipe data string dengan format huruf kapital
+      3. %b di gunakan untuk mencetak tipe data boolean dengan output true dan false*/
+        System.out.printf("%nNama : %S %nUmur ku 19 : %b", "puput yunita",19 );
+        
+    }
+    
+}
