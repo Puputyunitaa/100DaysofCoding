de1a33c37fee309cea9d3740dd36ec9b82eded15
Puputyunitaa <puputyunita1775@gmail.com>
2024-09-08 22:29:02 +0800

    Variabel kosong

--- Day6.java
+++ Day6.java
@@ -1,21 +1,8 @@
-//bagian kelas
-public class D7 {
-    //method main
-    public static void main(String[] args) {
-        
-        //deklarasi variabel kosong bertipe data string 
+
+public class Main {
+   //void main 
+   public static void main(String[] args) {
+        //deklarasi variabel kosong bertipe data string
         String jurusan;
-        
-        //inisialisasi  nilai variabel bertipe data string
-        jurusan = "informatika";
-        
-      //pendeklarasian variabel tinggi bertipe data short dengan memberikan nilai awal secara langsung 
-      short tinggi = 157;
-      
-      //output dari variabel yang kita buat
-        System.out.println("jurusan saya : " + jurusan);
-        System.out.println("tinggi badan : " + tinggi+ "cm");
-        
-    }
-    
+}
 }
