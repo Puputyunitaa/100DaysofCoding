0b3ff687a5b33b177de98a0b267668528611c7d9
Puputyunitaa <puputyunita1775@gmail.com>
2024-09-08 22:24:33 +0800

    inisialisasi variabel

--- Day7.java
+++ Day7.java
@@ -2,12 +2,20 @@
 public class D7 {
     //method main
     public static void main(String[] args) {
-       // //deklarasi variabel kosong bertipe data string 
+        
+        //deklarasi variabel kosong bertipe data string 
         String jurusan;
-        //deklarasi nilai variabel bertipe data string
+        
+        //inisialisasi  nilai variabel bertipe data string
         jurusan = "informatika";
+        
       //pendeklarasian variabel tinggi bertipe data short dengan memberikan nilai awal secara langsung 
       short tinggi = 157;
+      
+      //output dari variabel yang kita buat
+        System.out.println("jurusan saya : " + jurusan);
+        System.out.println("tinggi badan : " + tinggi+ "cm");
+        
     }
     
 }
