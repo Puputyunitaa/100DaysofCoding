565bcd949d6aa3ab79726f59c488076474f9054d
Puputyunitaa <puputyunita1775@gmail.com>
2024-09-09 20:31:40 +0800

    update nilai variabel

---
+++ Day8.java
@@ -0,0 +1,29 @@
+//bagian kelas
+public class D8 {
+    
+    //method main
+    public static void main(String[] args) {
+        
+        //pendeklarasian inisialisasi bertipe data String
+        String buah = "Durian";
+        
+        //output dari variabel yang belum di update
+        System.out.println("buah : " + buah);
+        
+        //update dengan mengganti nilai dari variabel buah
+               buah = "semangka";
+               
+        //output update dari variabel buah        
+        System.out.println("buah : " + buah);
+        
+        //update variable angka dengan nilai awal 123 di ubah menjadi 17
+        byte angka = 123;
+             angka = 17;
+             
+        //output update dari variabel yang kita buat    
+         System.out.println("angka : "+ angka);
+        
+        
+    }
+    
+}
