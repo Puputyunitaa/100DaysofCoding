98eb18e963b5826d2e5e848b60d4f9662c52078c
Puputyunitaa <puputyunita1775@gmail.com>
2024-09-10 23:11:04 +0800

    variabel kostanta

---
+++ Days9.java
@@ -0,0 +1,13 @@
+public class days9 {
+public static void main(String[] args) {
+
+        // deklarasi variabel kostanta
+        final String nama = "Puput Yunita";
+        final String nim = "D0223524";
+        final String kelas = "Inf C";
+        final String prodi = "Informatika";
+
+        // memanggil inputan dengan satu baris
+        System.out.println(nama + ", " + nim + ", " + kelas + ", " + prodi);
+    }
+}
