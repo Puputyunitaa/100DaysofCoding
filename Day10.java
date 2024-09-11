be3e1a5e6c9658f5ba0a53c2cb8113dce508329c
Puputyunitaa <puputyunita1775@gmail.com>
2024-09-11 17:37:29 +0800

    Tipe data byte

---
+++ Day10.java
@@ -0,0 +1,17 @@
+//bagin kelas
+public class Day10 {
+
+    //method main
+    public static void main(String[] args) {
+
+         // Batas minimal tipe data byte
+        byte minByte = Byte.MIN_VALUE;
+        System.out.println("Nilai minimum byte: " + minByte);
+
+        // Batas maksimal tipe data byte
+        byte maxByte = Byte.MAX_VALUE;
+        System.out.println("Nilai maksimum byte: " + maxByte);
+        
+    }
+    
+}
