e77a27fab81655c94cddf516b6a9402f9f7df316
Puputyunitaa <puputyunita1775@gmail.com>
2024-09-12 19:43:04 +0800

    tipe data short

---
+++ Day11.java
@@ -0,0 +1,13 @@
+//bagian kelas
+public class Day11 {
+    //method main
+    public static void main(String[] args) {
+        
+        //deklarsi variabel umur dengan tipe data short
+        short umur = 19;
+        
+        //outputnya
+        System.out.println("umur saya : " + umur);
+    }
+    
+}
