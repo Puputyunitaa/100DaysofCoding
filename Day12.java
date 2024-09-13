cf0341ec539da529e0323e7b76746ca74271695f
Puputyunitaa <puputyunita1775@gmail.com>
2024-09-13 21:23:58 +0800

    Tipe data int

---
+++ Day12.java
@@ -0,0 +1,14 @@
+// bagian class
+public class Day12 {
+
+    // method main
+    public static void main(String[] args) {
+    
+    // deklarasi variabel tinggi dengan tipe data integer
+    int tinggi = 160;
+    
+        // outputnya
+        System.out.println("tinggi saya adalah " + tinggi + " cm");
+        
+}
+}
