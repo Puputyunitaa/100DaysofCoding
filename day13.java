c6c90a67114fe6812c15e676983d7d098d5174c0
Puputyunitaa <puputyunita1775@gmail.com>
2024-09-14 20:41:27 +0800

    evaluasi

---
+++ day13.java
@@ -0,0 +1,26 @@
+public class D13 {
+    public static void main(String[] args) {
+        System.out.printf("Nama %S","pupuy");
+        System.out.println("");
+        System.out.printf("NIM %S" , "D0223524");
+        System.out.println("");
+        System.out.printf("Umur %d", 19);
+        
+        //deklarasi menuliskan variabel tampa nilai 
+        /*inisialisasi memberiakn nilai pada variabel*/
+        
+        final String nama;
+        
+         int aa = 4;
+         int bb = 5;
+         int cc = aa;
+            aa = bb;
+            bb = cc; 
+            
+            
+         System.out.println( aa);
+         System.out.println(bb);
+        
+    }
+    
+}
