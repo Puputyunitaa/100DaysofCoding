6bae413e8a7d4e2dbb26340dbb165153377c3d76
Puputyunitaa <puputyunita1775@gmail.com>
2024-09-15 21:01:18 +0800

    tipe data long

---
+++ Day14.java
@@ -0,0 +1,14 @@
+//bagian  kelas
+public class D14 {
+     
+    //method main
+    public static void main(String[] args) {
+        
+        //deklarasi dan inisialisasi tipe data long
+        long number = 17072258L;
+        
+        //outputnya
+        System.out.println("NO : " + number);
+    }
+    
+}
