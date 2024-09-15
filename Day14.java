06db21d4de26c9b7f34b4ba12e454be9aba584f5
Puputyunitaa <puputyunita1775@gmail.com>
2024-09-15 21:06:46 +0800

    Tipe data long

--- Day14.java
+++ Day14.java
@@ -4,7 +4,7 @@ public class D14 {
     //method main
     public static void main(String[] args) {
         
-        //deklarasi dan inisialisasi tipe data long dengan nama variabel long
+        //deklarasi dan inisialisasi tipe data long dengan nama variabel number 
         long number = 17072258L;
         
         //outputnya
