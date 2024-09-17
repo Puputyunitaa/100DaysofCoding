dcb25eebb1f9c27246e051ba8fd7788c57b4207e
Puputyunitaa <puputyunita1775@gmail.com>
2024-09-17 20:35:58 +0800

    tipe data float

---
+++ Day16.java
@@ -0,0 +1,13 @@
+//bagian kelas
+public class D16 {
+    //method main
+    public static void main(String[] args) {
+        
+        //deklarasi tipe data float dengan nama variabel harga
+        float harga = 89.999f;
+        
+        //outputnya
+        System.out.println("Harga Barang : " + harga);
+    }
+    
+}
