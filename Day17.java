6ada29b8d42d7d107b08bf6f6f04093cfb7fca99
Puputyunitaa <puputyunita1775@gmail.com>
2024-09-18 21:39:06 +0800

    tipe data char

---
+++ Day17.java
@@ -0,0 +1,27 @@
+public class D17 {
+    public static void main(String[] args) {
+       
+        char nilail = 'a';       
+        char nilai2 = 'b';
+        char nilai3 = 'c';
+             nilai2 = nilai3;
+             
+        final char nilai4 = 'd';
+        char kosong;
+        kosong =  nilail;
+        nilai2 = kosong;
+        nilail = nilai3;
+        nilai3 = nilai4;
+       
+              
+        
+        System.out.println(nilail);
+        System.out.println(nilai2);
+        System.out.println(nilai3);
+             
+        
+    }
+}
+
+   
+   
