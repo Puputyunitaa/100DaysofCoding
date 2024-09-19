509e4e4a821a045b97ffb424ff141b46a44f872d
Puputyunitaa <puputyunita1775@gmail.com>
2024-09-19 21:31:31 +0800

    tipe data boolean

---
+++ Day18.java
@@ -0,0 +1,11 @@
+public class D18 {
+    public static void main(String[] args) {
+        
+         boolean itu = false;
+         boolean ini = true;
+         
+         System.out.println("saya pendek = " + itu);
+         System.out.println("saya tinggi = " + ini);
+    }
+    
+}
