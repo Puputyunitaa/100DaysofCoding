2912d359958c8fd67657a3b717321b39c015267b
Puputyunitaa <puputyunita1775@gmail.com>
2024-09-20 23:45:37 +0800

    String reference

---
+++ Day19.java
@@ -0,0 +1,12 @@
+public class D19 {
+    public static void main(String[] args) {
+        
+       String a = "pasti bisa";
+        String b = a;
+
+       
+         a = "aku bisa"; 
+        System.out.println(a); 
+        System.out.println(b); 
+    }
+}
