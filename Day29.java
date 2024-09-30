1455657c18eba69fa97c3a0aebb5c140d2f705de
Puputyunitaa <puputyunita1775@gmail.com>
2024-09-30 22:12:21 +0800

    BigInteger

---
+++ Day29.java
@@ -0,0 +1,13 @@
+import java.math.BigInteger;
+
+public class D29 {
+    public static void main(String[] args) {
+      
+         BigInteger pu = new BigInteger("1234567890");
+         BigInteger pu2 = BigInteger.valueOf(12345l);
+
+         System.out.println(pu);
+         System.out.println(pu2);
+    }
+    
+}
