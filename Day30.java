10d30a10da2e532fece6a2e73c3d50ddf47ae518
Puputyunitaa <puputyunita1775@gmail.com>
2024-10-01 21:38:10 +0800

    BigDecimal

---
+++ Day30.java
@@ -0,0 +1,11 @@
+import java.math.BigDecimal;
+
+public class D30 {
+    public static void main(String[] args) {
+        BigDecimal a = new BigDecimal("177");
+        BigDecimal b = new BigDecimal("277");
+        BigDecimal c = a.add(b); // Penjumlahan
+        System.out.println("Hasil: " + c); 
+    }
+    
+}
