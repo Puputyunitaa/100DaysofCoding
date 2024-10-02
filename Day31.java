f6ff91acef333b42a25cccfaac88ad632455c05e
Puputyunitaa <puputyunita1775@gmail.com>
2024-10-02 21:26:00 +0800

    konversi String ke  tipe primitif

---
+++ Day31.java
@@ -0,0 +1,35 @@
+        String huruf = "puput";
+        char charString = huruf.charAt(0);
+        System.out.println(charString);
+
+        String angka1 = "90";
+        int intString = Integer.parseInt(angka1);
+        System.out.println(intString);
+
+        String angka2 = "50";
+        short shortString = Short.parseShort(angka2);
+        System.out.println(shortString);
+
+        String angka3 = "60";
+        byte byteString = Byte.parseByte(angka3);
+        System.out.println(byteString);
+
+        String angka4 = "70.9";
+        float floatString = Float.parseFloat(angka4);
+        System.out.println(floatString);
+
+        String angka5 = "100";
+        long longString = Long.parseLong(angka5);
+        System.out.println(longString);
+
+        String angka6 = "80.5";
+        double doubleString = Double.parseDouble(angka6);
+        System.out.println(doubleString);
+
+        String angka7 = "true";
+        boolean booleanString = Boolean.parseBoolean(angka7);
+        System.out.println("pengen kaya : " + booleanString);
+
+    }
+
+}
