d1f26a25904c71261bc8071fed7c3a62b80172d6
Puputyunitaa <puputyunita1775@gmail.com>
2024-10-03 21:21:53 +0800

    reference tipe primitif ke  String

---
+++ Day32.java
@@ -0,0 +1,39 @@
+public class D32 {
+    public static void main(String[] args) {
+        int a = 90;
+        String aa = String.valueOf(a);
+        System.out.println("int : " + aa);
+        
+        short b = 30;
+        String bb = String.valueOf(b);
+        System.out.println("short : " + bb);
+        
+        byte c = 20;
+        String cc = String.valueOf(b);
+        System.out.println("byte : " + cc);
+        
+        long d = 900l;
+        String dd = String.valueOf(d);
+        System.out.println("long : " + dd);
+        
+        float e = 70.8f;
+        String ee = String.valueOf(e);
+        System.out.println("float : " + ee);
+        
+        double g = 60.5;
+        String gg = String.valueOf(g);
+        System.out.println("double : " + gg);
+        
+        char h = 'p';
+        String hh = String.valueOf(h);
+        System.out.println("char : " + hh);
+        
+        boolean i = true;
+        String ii = String.valueOf(i);
+        System.out.println("boolean : " + ii);
+        
+        
+        
+    }
+    
+}
