f0db65f8b386b656e7a317e6d633d3499443b8b4
Puputyunitaa <puputyunita1775@gmail.com>
2024-10-05 22:46:03 +0800

    input dari keyboard

---
+++ Day34.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+public class D34 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Nama : ");
+        String nama = input.nextLine();
+        System.out.print("Nim : ");
+        String nim = input.nextLine();
+        System.out.print("Umur: ");
+        short umur = input.nextShort();
+        System.out.print("Alamat: ");
+        String alamat = input.nextLine();
+        input.nextLine();
+        System.out.print("Kelas : ");
+        char kelas = input.next().charAt(0);
+        System.out.print("Berat Badan: ");
+        float bb = input.nextFloat();
+        System.out.print("Tinggi Badan: ");
+        int tinggi = input.nextInt();
+        System.out.print("motivasi : ");
+        String Motivasi = input.nextLine();
+         input.nextLine();
+        System.out.print("pesan dan Kesan : ");
+        String PesandanKesan = input.next();
+        
+        
+    }
+    
+}
