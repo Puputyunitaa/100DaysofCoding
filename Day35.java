3b1f7e2b72c04a0aefe2d134e00721236af8c885
Puputyunitaa <puputyunita1775@gmail.com>
2024-10-06 22:20:09 +0800

    operator aritmatika penjumlahan dan pengurangan

---
+++ Day35.java
@@ -0,0 +1,14 @@
+public class D35 {
+    public static void main(String[] args) {
+        int p = 100;
+        int u = 20;
+        
+        int tambah = p + u;
+        System.out.println(tambah);
+        
+        int kurang = p - u;
+        System.out.println(kurang);
+        
+    }
+    
+}
