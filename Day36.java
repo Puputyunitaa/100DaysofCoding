4e312b8c4cc63ddd20144c2da86060cfe099fba0
Puputyunitaa <puputyunita1775@gmail.com>
2024-10-07 19:01:05 +0800

    bagi, kali, modulo

---
+++ Day36.java
@@ -0,0 +1,17 @@
+public class D36 {
+
+    public static void main(String[] args) {
+        int aa = 5;
+        int bb = 2;
+
+        int kali = aa * bb;
+        System.out.println("hasil kali = " + kali);
+
+        int bagi = aa / bb;
+        System.out.println("hasil bagi = " + bagi);
+
+        int modulo = aa % bb;
+        System.out.println("hasil modulus = " + modulo);
+    }
+
+}
