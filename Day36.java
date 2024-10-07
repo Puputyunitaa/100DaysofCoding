454f65d8e2421ade21b2fc986224743661b8388a
Puputyunitaa <puputyunita1775@gmail.com>
2024-10-07 22:56:43 +0800

    bagi, kali, modulus

--- Day36.java
+++ Day36.java
@@ -1,17 +1,21 @@
 public class D36 {
 
     public static void main(String[] args) {
-        int aa = 5;
-        int bb = 2;
+        int a = 5;
+        int b = 2;
+        double aa = 5;
+        double bb = 2;
+        
 
-        int kali = aa * bb;
+        int kali = a * b;
         System.out.println("hasil kali = " + kali);
 
-        int bagi = aa / bb;
+        double bagi = aa / bb;
         System.out.println("hasil bagi = " + bagi);
 
-        int modulus = aa % bb;
+        int modulus = a % b;
         System.out.println("hasil modulus = " + modulus);
     }
 
 }
+
