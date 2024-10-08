7a11651543aea6b71162384532442c7c76337efe
Puputyunitaa <puputyunita1775@gmail.com>
2024-10-08 21:25:41 +0800

    operator penugasan, pengisian, tambah, kurang

---
+++ Day37.java
@@ -0,0 +1,15 @@
+public class D37 {
+
+    public static void main(String[] args) {
+        byte put = 40;
+        System.out.println("pengisian nilai : " + put);
+
+        put += 7;
+        System.out.println("pennambahan nilai : " + put);
+
+        put -= 10;
+        System.out.println("pengurangan niali : " + put);
+
+    }
+
+}
