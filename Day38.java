db85a540afd6f977135fbdf02e72722a44136146
Puputyunitaa <puputyunita1775@gmail.com>
2024-10-09 20:03:01 +0800

    operator penugasan nilai, perkalian, pembagian

---
+++ Day38.java
@@ -0,0 +1,13 @@
+public class D38 {
+    public static void main(String[] args) {
+        int p = 80;
+        System.out.println("pengisian nilai = " + p);
+        
+          p *= 4;
+          System.out.println("perkalian = " + p);
+          
+          p /= 2;
+          System.out.println("pembagian = " +  p);
+    }
+    
+}
