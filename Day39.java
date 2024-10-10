d017d1ecd1db394be6385633be5b4f1ae95f8b26
Puputyunitaa <puputyunita1775@gmail.com>
2024-10-10 22:50:14 +0800

    Operator penugasan: modulus

---
+++ Day39.java
@@ -0,0 +1,8 @@
+public class Day39 {
+public static void main(String[] args) {
+   int modulus = 11;
+   modulus %= 2;
+   System.out.println("modulus atau sisa bagi adalah = " + modulus);
+}
+    
+}
