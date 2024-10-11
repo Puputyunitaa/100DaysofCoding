a76481a329ed98dc0b109544341bca4199642ccb
Puputyunitaa <puputyunita1775@gmail.com>
2024-10-11 22:46:48 +0800

    operator perbandinga lebih besar, lebih kecil

--- Day40.java
+++ Day40.java
@@ -2,16 +2,10 @@ public class D40 {
     public static void main(String[] args) {
         int p = 20;
         int pu =10;
-        if (p > pu) {
-            System.out.println("niali p lebih besar dari nilai pu" );
-        } else {
-            System.out.println("nilai p tidak lebih besar dari pu ");
-        }
-        if (p < pu) {
-            System.out.println("p lebih kecil dari pu");
-        } else {
-            System.out.println("nilai p tidak lebih kecil dari nilai pu ");
-        }
+       
+        boolean u = p > pu;
+        System.out.println("nilai p lebih besar dari nilai pu = " + u);
+        boolean t = p < pu;
+        System.out.println("nilai p lebih kecil dari nilai pu = " + t);
     }
 }
-   
