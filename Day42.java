1ebda19b4b888fe0109022e4c5caa700de88cf5a
Puputyunitaa <puputyunita1775@gmail.com>
2024-10-13 23:17:27 +0800

    penugasan sama dengan tidak sama dengan

---
+++ Day42.java
@@ -0,0 +1,12 @@
+public class D42 {
+    public static void main(String[] args) {
+        int c = 7;
+        int d = 8;
+        boolean e = c == d;
+        System.out.println(e);
+        boolean f = c!= d;
+        System.out.println(f);
+                
+    }
+    
+}
