9884fd7145c111ee56310d6ed53601b7f8a0022a
Puputyunitaa <puputyunita1775@gmail.com>
2024-10-14 23:20:44 +0800

    operator perbandinga lebih besar sama denggan dan, lebih kecil sama dengan

---
+++ Day43.java
@@ -0,0 +1,11 @@
+public class D43 {
+    public static void main(String[] args) {
+        int i = 10;
+        int s = 11;
+        boolean j = i <= s;
+        System.out.println(j);
+         boolean p = i >= s;
+        System.out.println(p);
+    }
+    
+}
