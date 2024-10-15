6909e603eac9b352fc375459e294cdfe16a6003c
Puputyunitaa <puputyunita1775@gmail.com>
2024-10-15 23:36:53 +0800

    operator logika AND

---
+++ Day44.java
@@ -0,0 +1,10 @@
+public class D44 {
+    public static void main(String[] args) {
+           int a = 4;
+           int b = 5;
+           System.out.println( a < 5 && a > b);
+           
+                   
+    }
+    
+}
