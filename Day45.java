793a9cdf992c1e014f8df716ec1e79b18216fb7a
Puputyunitaa <puputyunita1775@gmail.com>
2024-10-16 22:29:32 +0800

    operator logika OR

---
+++ Day45.java
@@ -0,0 +1,12 @@
+public class D45 {
+    public static void main(String[] args) {
+       short pi = 3;
+       short di = 7;
+       
+        System.out.println(pi >= di || pi > di);
+        System.out.println(di == pi || pi < di);
+       
+       
+    }
+    
+}
