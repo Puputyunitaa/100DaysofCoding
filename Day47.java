942cb44210dfe31d7c128ab82f0f28ef88acafbc
Puputyunitaa <puputyunita1775@gmail.com>
2024-10-18 20:24:01 +0800

    operator percabangan if

---
+++ Day47.java
@@ -0,0 +1,17 @@
+public class D47 {
+    public static void main(String[] args) {
+        int nilai = 600;
+        
+        if (nilai > 0) {
+            System.out.println("Niali lebih besar dari Nol.");
+        }
+
+        if (nilai < 0) {
+            System.out.println("Nilai lebih kecil dari Nol.");
+        }
+
+        if (nilai == 0) {
+            System.out.println("Niali sma dengan Nol");
+        }
+    }
+}
