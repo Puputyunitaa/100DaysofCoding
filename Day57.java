30ba3a716c98c1158bed86f8fb2eca04c6e1f117
Puputyunitaa <puputyunita1775@gmail.com>
2024-10-28 22:54:42 +0800

    Loop increment

---
+++ Day57.java
@@ -0,0 +1,13 @@
+public class D57 {
+    public static void main(String[] args) {
+      
+        int p = 7; 
+
+       
+        while(p <= 17) {
+             
+            System.out.println(p); 
+            p++;
+        }
+    }
+}
