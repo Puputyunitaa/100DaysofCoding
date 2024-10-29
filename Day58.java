0de42d87808934c85afbdcad7e1d2359fdee223e
Puputyunitaa <puputyunita1775@gmail.com>
2024-10-29 22:54:44 +0800

    lopp while decrement

---
+++ Day58.java
@@ -0,0 +1,13 @@
+public class D58 {
+    public static void main(String[] args) {
+       byte p = 50; 
+
+       
+        while(p >= 0) {
+             if(p %2!= 0)
+            System.out.println(p); 
+            p--;
+        }
+    }
+}
+   
