2f20a30bbb3952b8f42b0b9ba9be622a6d38c305
Puputyunitaa <puputyunita1775@gmail.com>
2024-10-30 23:42:39 +0800

    Looping for each

---
+++ Day59.java
@@ -0,0 +1,10 @@
+public class D59 {
+    public static void main(String[] args) {
+ 
+        String[] jurusan = {"Informatikan", "Hukum", "Ilmu Politik", "Ekonomi"};
+
+        for (String p : jurusan) {
+            System.out.println(p);
+        }
+    }
+}
