2e790286d33d3e124abf64ed19ed4412e3d61a5c
Puputyunitaa <puputyunita1775@gmail.com>
2024-11-08 22:09:56 +0800

    method non void

---
+++ Day68.java
@@ -0,0 +1,17 @@
+
+public class D68 {
+
+    public static void main(String[] args) {
+        D68 p = new D68();
+        System.out.println(p.perkalian());
+               
+    }
+    int perkalian(){
+        System.out.print("Hasil : ");
+        return 17*7;
+        
+    }
+}
+
+
+
