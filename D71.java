409e2d20a321e6f491c303d86da50b6c1de1f330
Puputyunitaa <puputyunita1775@gmail.com>
2024-11-11 23:47:11 +0800

    method static

---
+++ D71.java
@@ -0,0 +1,15 @@
+public class D71 {    
+
+     static void tampilkanAngkaGanjil() {
+        for (int i = 1; i <= 10; i++) {
+            if (i % 2 != 0) { 
+                System.out.println(i);
+            }
+        }
+    }
+   
+    
+    public static void main(String[] args) {
+        tampilkanAngkaGanjil();
+    }
+}
