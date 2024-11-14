f1f9492666d208adf66feb8c5fb78f22712477e8
Puputyunitaa <puputyunita1775@gmail.com>
2024-11-14 21:08:41 +0800

    String method : lengh.()

---
+++ Day74.java
@@ -0,0 +1,18 @@
+
+import java.util.Scanner;
+public class D74 {
+    public static void main(String[] args) {
+       
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Masukkan sebuah kata : ");
+        String kata = input.nextLine();
+        
+        int panjangString = kata.length();
+   
+        System.out.println("Panjang dari kata : " + panjangString);
+
+    }
+}
+    
+
