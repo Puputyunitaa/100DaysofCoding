c573f7742fb9adf5d67880cf2a50969caf9def83
Puputyunitaa <puputyunita1775@gmail.com>
2024-11-17 21:49:17 +0800

    String method : equals

---
+++ Day77.java
@@ -0,0 +1,14 @@
+public class D77 {
+public static void main(String[] args) {
+
+            String kata1 = "pipit";
+            String kata2 = "puput";
+            String kata3 = "pipit";
+
+            boolean hasil1 = kata1.equals(kata2);
+            boolean hasil2 = kata1.equals(kata3);
+
+            System.out.println("Apakah kata1 sama dengan kata2? :" + hasil1);
+            System.out.println("Apakah kata1 sama dengan kata3? :" + hasil2);
+        }
+    }
