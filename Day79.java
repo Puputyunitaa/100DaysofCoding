5be8e9aad5744e11d56d55e911e84fbf2f1a779c
Puputyunitaa <puputyunita1775@gmail.com>
2024-11-19 20:38:42 +0800

    String Method : toString()

---
+++ Day79.java
@@ -0,0 +1,38 @@
+public class Day79 {
+    public static void main(String[] args) {
+     
+        int i = 123;
+        String intkeString = Integer.toString(i);
+        System.out.println("int sebagai string : " + intkeString);
+
+        double d = 45.67;
+        String doublekeString = Double.toString(d);
+        System.out.println("double sebagai string : " + doublekeString);
+
+        float f = 12.34f;
+        String floatkeString = Float.toString(f);
+        System.out.println("float sebagai string : " + floatkeString);
+
+        long l = 12345L;
+        String longkeString = Long.toString(l);
+        System.out.println("long sebagai string : " + longkeString);
+
+        short s = 320;
+        String shortkeString = Short.toString(s);
+        System.out.println("short sebagai string : " + shortkeString);
+
+        byte b = 117;
+        String bytekeString = Byte.toString(b);
+        System.out.println("byte sebagai string : " + bytekeString);
+
+        char c = 'A';
+        String charkeString = Character.toString(c);
+        System.out.println("char sebagai string : " + charkeString);
+
+        boolean bo = true;
+        String booleankeString = Boolean.toString(bo);
+        System.out.println("Boolean sebagai string : " + booleankeString);
+
+       
+    }
+}
