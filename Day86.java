c8f0d892f66f0c5a9df025e92979fb6743351cc0
Puputyunitaa <puputyunita1775@gmail.com>
2024-11-26 21:36:01 +0800

    mencetak elemen dalam array

---
+++ Day86.java
@@ -0,0 +1,12 @@
+public class D86 {
+
+    public static void main(String[] args) {
+        int ukuran = 5;
+        int[] array = {10, 20, 30, 40, 50};
+
+        System.out.println("Elemen-elemen dalam array :");
+        for (int i = 0; i < ukuran; i++) {
+            System.out.println("Elemen ke-" + (i + 1) + " : " + array[i]);
+        }
+    }
+}
